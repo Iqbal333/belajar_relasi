245bbcd2a497c729893051c5fe3c8e2fe8f70333
Iqbal333 <iqbalgooner33@gmail.com>
2020-09-03 18:36:23 +0700

    Membuat form edit

--- resources/views/Bensin/edit.blade.php
+++ resources/views/Bensin/edit.blade.php
@@ -11,3 +11,31 @@
             {!! $errors->has('nama')?$errors->first('nama'):'' !!}
         </div>
     </div>
+    <div class="form-group">
+        {!! Form::label('harga', 'Harga', ['class'=>'control-label col-md-2']) !!}
+        <div class="col-md-10">
+            {!! Form::number('harga', null, ['class'=>'control-label col-md-2']) !!}
+            {!!  $errors->has('harga')?$errors->first('harga'):'' !!}
+        </div>
+    </div>
+    <div class="form-group">
+        {!! Form::label('ron', 'RON', ['class'=>'control-label col-md-2']) !!}
+        <div class="col-md-10">
+            {!! Form::text('ron', null, ['class'=>'form-control']) !!}
+            {!! $errors->has('ron')?$errors->first('ron'):'' !!}
+        </div>
+    </div>
+    <div class="form-group">
+        {!! Form::label('id_vehicle', 'Jenis', ['class'=>'control-label col-md-2']) !!}
+        <div class="col-md-10">
+            {!! Form::number('id_vehicle', null, ['class'=>'form-control']) !!}
+            {!! $errors->has('id_vehicle')?$errors->first('id_vehicle'):'' !!}
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-md-offset-2 col-md-10">
+            {!! Form::submit('Simpan', ['class'=>'btn btn-primary']) !!}
+        </div>
+    </div>
+    {!! Form::close !!}
+@stop
